531e4d74ed46fe303f31c5538fe90e38f3668fe8
kailashrajput73 <praveen772rajput@gmail.com>
2025-09-10 18:29:35 +0530

    again chacking

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,16 @@ import { StyleSheet, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View>
+    <View style={styles.box}>
       
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  box:{
+    backgroundColor:"red",
+    height:100,
+    width:100,
+  },
 });
